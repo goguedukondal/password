8dd0ecc6c57f247222098264f243abac73e4cc18
GOGU EDUKONDAL <97788201+goguedukondal@users.noreply.github.com>
2022-12-29 19:12:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import PasswordGen from './components/PasswordGen';
+
+function App() {
+  return (
+    <div className="App">
+      <PasswordGen/>
+      
+    </div>
+  );
+}
+
+export default App;
